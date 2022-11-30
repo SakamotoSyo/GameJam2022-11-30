7f0d3f996e352e5dbb59805512bd54741dc683e5
Aiba_0325 <bantan.ab325@gmail.com>
2022-11-30 17:30:35 +0900

    [Add]　イベント

---
+++ Assets/InGame/Aiba/LightOff.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightOff : MonoBehaviour
+{
+
+    public void Off()
+    {
+        this.gameObject.SetActive(false);
+    }
+}
