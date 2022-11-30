7ebe5ca9d707f985871297b159b53d5f852d619f
sakamotosyo <syo10150000@gmail.com>
2022-11-30 17:30:37 +0900

    [Update]PlayerにAniamtionの追加

--- Assets/InGame/Sakamoto/Script/PlayerAnimation.cs
+++ Assets/InGame/Sakamoto/Script/PlayerAnimation.cs
@@ -6,10 +6,33 @@ using UnityEngine;
 public class PlayerAnimation
 {
     [SerializeField] Animator _anim;
+    
+    PlayerInput _input;
 
-    void Update()
+    public void Init(PlayerInput playerInput) 
     {
-        
+        _input = playerInput;
+    }
+    public void Update()
+    {
+        if (_input.MoveInput == Vector2.zero)
+        {
+            _anim.SetBool("Walk", false);
+        }
+        else
+        {
+            _anim.SetBool("Walk", true);
+        }
+
+        if (_input.Action)
+        {
+            _anim.SetBool("Punch", true);
+        }
+        else 
+        {
+            _anim.SetBool("Punch", false);
+        }
+        _anim.SetFloat("X", _input.LastMoveDir.x);
     }
 
     
--- Assets/InGame/Sakamoto/Script/PlayerMovementController.cs
+++ Assets/InGame/Sakamoto/Script/PlayerMovementController.cs
@@ -8,17 +8,20 @@ public class PlayerMovementController : MonoBehaviour, IDamage
     [SerializeField] private PlayerInput _playerInput;
     [SerializeField] private PlayerMove _playerMove;
     [SerializeField] private PlayerDamage _playerDamage;
+    [SerializeField] private PlayerAnimation _playerAnim;
     [SerializeField] private PlayerPhotographed _playerPhotographed;
     private void Start()
     {
         _playerMove.Init(_playerInput);
         _playerPhotographed.Init(_playerInput);
+        _playerAnim.Init(_playerInput);
     }
 
     private void Update()
     {
         _playerInput.Update();
         _playerMove.Update(transform);
+        _playerAnim.Update();
     }
 
     private void FixedUpdate()
