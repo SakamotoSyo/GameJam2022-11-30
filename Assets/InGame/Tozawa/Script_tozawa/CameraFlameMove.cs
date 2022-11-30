6396257964ec9a30dc5b975d1b3b6a015e1608f3
st37071556@icloud.com <st37071556@icloud.com>
2022-11-30 14:31:19 +0900

    移動と待機を繰り返すFlameMoveを作成

---
+++ Assets/InGame/Tozawa/Script_tozawa/CameraFlameMove.cs
@@ -0,0 +1,95 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+/// <summary>
+/// フレームの移動を管理する
+/// </summary>
+public class CameraFlameMove : MonoBehaviour
+{
+    const int NUM_HALF = 2;
+    [SerializeField, Header("カメラフレームX軸下上限")]
+    int _xRange;
+    [SerializeField, Header("カメラフレームY軸下上限")]
+    int _yRange;
+    [SerializeField, Range(1, 5),Header("カメラフレーム1辺の大きさ")]
+    int _flameLength = 1;
+    Vector3 _flameSize;
+    Vector3 _moveStartPos;
+    Vector3 _moveEndPos;
+    [SerializeField,Header("移動速度")]
+    float _moveSpeed;
+    [SerializeField, Header("撮影インターバル")]
+    float _takePhotoInterval;
+    float _moveDistance;
+    float _CameraXPos;
+    float _CameraYPos;
+    float _time;
+    float presentPos;
+    [SerializeField]bool _isMove = false;
+    const int NUM_ONE = 1;
+    void Start()
+    {
+        SizeChange();//フレームの大きさ変更
+        SetInitialPos();//初期位置設定
+    }
+    void Update()
+    {
+        if(_isMove == false)//移動してない時は撮影待機
+        {
+            CountTimer();
+        }
+        else
+        {
+            FlameMove();
+        }
+    }
+
+    private void CountTimer()
+    {
+        _time += Time.deltaTime;
+        if(_time >= _takePhotoInterval)
+        {
+            SetMoveStatus();
+            _isMove = true;
+            _time = 0;
+        }
+    }
+
+    void SetMoveStatus()//移動のための位置定義
+    {
+        _moveStartPos = this.transform.position;//開始地点(現在地)決定
+        _moveEndPos = RandomPos();//次の移動場所
+        _moveDistance = Vector3.Distance(_moveStartPos,_moveEndPos);//二点間の距離
+    }
+    void FlameMove()//移動
+    {
+        _time += Time.deltaTime;
+        presentPos = (_time * _moveSpeed) / _moveDistance;
+        transform.position = Vector3.Slerp(_moveStartPos, _moveEndPos, presentPos);
+        if(presentPos >= NUM_ONE)
+        {
+            presentPos = 0;
+            _time = 0;
+            _isMove = false;
+        }
+    }
+
+    void SetInitialPos()//初期位置設定
+    {
+        gameObject.transform.position = RandomPos();
+    }
+
+    Vector3 RandomPos()//画面内のランダムな座標を返す（Vector2）
+    {
+        _CameraXPos = Random.Range(-_xRange,_xRange);
+        _CameraYPos = Random.Range(-_yRange,_yRange);
+        return new Vector3(_CameraXPos,_CameraYPos);
+    }
+    void SizeChange()//フレームの大きさ変更
+    {
+        _flameSize = new Vector3(_flameLength, _flameLength, _flameLength);
+        gameObject.transform.localScale = _flameSize;
+    }
+}
